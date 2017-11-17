8265fbe8b8081135282f576a019ade26370e73cc
Andrew <colvinwandrew@gmail.com>
2017-11-16 18:29:55 -0600

    More Network Testing

--- ResourceMonitor/ResourceMonitor/Form1.cs
+++ ResourceMonitor/ResourceMonitor/Form1.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
 
 namespace ResourceMonitor
 {
     public partial class ResourceMonitor : Form
     {
         public ResourceMonitor() => InitializeComponent();
+
+        float previousU = 0;
+        float previousD = 0;
 
         //Update Info per Second
         private void Timer_Tick(object sender, EventArgs e)
@@ -21,8 +25,15 @@ namespace ResourceMonitor
             float cpu = cpuPerf.NextValue();
             float ramAv = ramPerfAv.NextValue();
             float ramPerc = ramPerfPerc.NextValue();
-            float upload = upPerf.NextValue();
-            float download = downPerf.NextValue();
+            //float upload = upPerf.NextValue();
+            //float download = downPerf.NextValue();
+
+            IPv4InterfaceStatistics network;
+            network = NetworkInterface.GetAllNetworkInterfaces()[0].GetIPv4Statistics();
+            float upload = network.BytesSent - previousU;
+            float download = network.BytesReceived - previousD;
+            previousU = network.BytesSent;
+            previousD = network.BytesReceived;
 
             //Bar values
             cpuBar.Value = (int)cpu;
@@ -31,7 +42,10 @@ namespace ResourceMonitor
             //Text Values
             cpuText.Text = string.Format("{0:0.00}%", cpu);
             ramText.Text = string.Format("{0:0.00} GB FREE", ramAv/1000);
+ //           upText.Text = string.Format("{0:0.00} b/s", upload);
+ //           downText.Text = string.Format("{0:0.00} b/s", download)
 
+            
             //Upload Text Values
             if(upload > 1000000000.00)
                 upText.Text = string.Format("{0:0.00} Gb/s", upload / 1000000000.00);
