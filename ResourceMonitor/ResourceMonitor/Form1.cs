53c1cdbd2d0300079d93c2f60d600d8b3ec5e1d9
Andrew <colvinwandrew@gmail.com>
2017-12-06 15:38:45 -0600

    Network Working

--- ResourceMonitor/ResourceMonitor/Form1.cs
+++ ResourceMonitor/ResourceMonitor/Form1.cs
@@ -17,6 +17,10 @@ namespace ResourceMonitor
 
         float previousU = 0;
         float previousD = 0;
+        float tempU = 0;
+        float tempD = 0;
+        float upload = 0;
+        float download = 0;
 
         //Update Info per Second
         private void Timer_Tick(object sender, EventArgs e)
@@ -25,15 +29,18 @@ namespace ResourceMonitor
             float cpu = cpuPerf.NextValue();
             float ramAv = ramPerfAv.NextValue();
             float ramPerc = ramPerfPerc.NextValue();
-            //float upload = upPerf.NextValue();
-            //float download = downPerf.NextValue();
 
             IPv4InterfaceStatistics network;
             network = NetworkInterface.GetAllNetworkInterfaces()[0].GetIPv4Statistics();
-            float upload = network.BytesSent - previousU;
-            float download = network.BytesReceived - previousD;
-            previousU = network.BytesSent;
-            previousD = network.BytesReceived;
+
+            //convert to Bits
+            tempU = network.BytesSent * 8;
+            tempD = network.BytesReceived * 8;
+
+            upload = tempU - previousU;
+            download = tempD - previousD;
+            previousU = tempU;
+            previousD = tempD;
 
             //Bar values
             cpuBar.Value = (int)cpu;
@@ -42,8 +49,6 @@ namespace ResourceMonitor
             //Text Values
             cpuText.Text = string.Format("{0:0.00}%", cpu);
             ramText.Text = string.Format("{0:0.00} GB FREE", ramAv/1000);
- //           upText.Text = string.Format("{0:0.00} b/s", upload);
- //           downText.Text = string.Format("{0:0.00} b/s", download)
 
             
             //Upload Text Values
